aea806a51d3492087b894b9024009ea09a616e1f
Vibhav Chaturvedi <chaturvedi_v@India.XoriantCorp.com>
2021-05-27 22:32:46 +0530

    refactor(coutSortPython.py): make countsort function
    * added countSort function
    * added Doc string to the function
    * change variable names
    
    :#fix1

--- countSortPython.py
+++ countSortPython.py
@@ -1,16 +1,27 @@
-a=list(map(int,input().split()))
-maxi=max(a)
-yo=[0 for i in range(0,maxi+1)]
-for i in range(0,len(a)):
-    yo[a[i]]+=1
-print(yo)    
-    
-g=0
-    
-for i in range(0,len(yo)):
-    while(yo[i]>0):
-        a[g]=i;
-        g=g+1
-        yo[i]=yo[i]-1
-print(a)        
-    
+def countSort(input_array):
+    """
+    this function implement the countsort sorting algo and return a sorted array
+
+    """
+    max_element = max(input_array)
+    initial_array = [0] * (max_element + 1)
+    for element in range(0, len(input_array)):
+        initial_array[input_array[element]] += 1
+
+    index = 0
+    for i in range(0, len(initial_array)):
+        while initial_array[i] > 0:
+            input_array[index] = i
+            index += 1
+            initial_array[i] -= 1
+
+    return input_array
+
+
+if __name__ == '__main__':
+    input_array = list(map(int, input("Enter the elements to be sorted! : \t\n").split()))
+    print("Array Before Sorting :\n \t")
+    print(input_array)
+    countSort(input_array)
+    print("Array After Sorting :\n \t")
+    print(input_array)
